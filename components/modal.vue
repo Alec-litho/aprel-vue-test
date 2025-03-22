ae908a0d099690c76100e5218826dbd008d84bcf
oleg <zustzero123@gmail.com>
2025-03-23 00:38:34 +0300

    modal created

---
+++ components/modal.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="modal-overlay" @click.self="$emit('close')">
+      <div class="modal-content">
+        <h2>{{ item.name.official }}</h2>
+        <img :src="item.flags.png" :alt="item.name.common + ' flag'">
+        <div class="details">
+          <p><strong>Region:</strong> {{ item.region }}</p>
+          <p><strong>Capital:</strong> {{ item.capital?.[0] || 'N/A' }}</p>
+          <p><strong>Population:</strong> {{ item.population.toLocaleString() }}</p>
+        </div>
+        <button class="btn" @click="$emit('close')">Close</button>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  defineProps<{
+    item: Country
+  }>()
+  
+  defineEmits(['close'])
+  </script>
